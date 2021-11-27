e0bd3093b01cf79b1900fc9d205cd9434ee7265f
Nathália Nogueira <94802675+NathaliaNogueira@users.noreply.github.com>
2021-11-26 23:31:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from models.cartao_credito import Cartao_Credito
 from models.pedido import Pedido
+from Livro import Livro
+from Game import Game
 
 cartao = Cartao_Credito(123456, 1025, 123, 5000)
 
@@ -22,3 +24,25 @@ print(pedido.get_numero())
 print('Pedido: ' + str(pedido.faturar_pedido(cartao)))
 print('Estornar Pedido :' + str(pedido.estornar(pedido)))
 print('Estornar Pedido :' + str(pedido.estornar(pedido1)))
+
+
+l1 = Livro(2121, "Senhor dos Anéis", "Tolkien", 40)
+l2 = Livro(3232, "Harry Potter", "JK Rowling", 45)
+
+g1 = Game("Need for Speed","EA", 22)
+g2 = Game("The Sims","EA", 30)
+
+
+print(l1.getEstoque())
+print(l2.getEstoque())
+print(g1.getEstoque())
+print(g2.getEstoque())
+print(g1.getTitulo())
+print(g2.getPreço())
+
+print(l2.getPreço())
+
+print(l1)
+print(l2)
+print(g1)
+print(g2)
