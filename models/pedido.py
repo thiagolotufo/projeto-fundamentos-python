bc2efc6195668fea432c44062c48fa0b9376cb67
Ana Espois <ana.espois@cortex-intelligence.com>
2021-12-01 13:49:30 -0300

    Integração

--- models/pedido.py
+++ models/pedido.py
@@ -2,37 +2,40 @@ from random import randint
 
 class Pedido:
     def __init__(self, cliente, itens, valor_total):
-        self._numero = randint(1000,2000)
+        self._numero = randint(1000, 2000)
         self._cliente = cliente
         self._itens = itens
         self._valor_total = valor_total
         self._status_pagamento = False
 
-    def get_numero(self):
+    def getNumero(self):
         return self._numero
 
-    def get_cliente(self):
+    def getCliente(self):
         return self._cliente
-    
-    def get_itens(self):
+
+    def getItens(self):
         return self._itens
 
-    def get_status_pagamento(self):
+    def getIValorTotal(self):
+        return self._valor_total
+
+    def getStatusPagamento(self):
         return self._status_pagamento
 
-    def set_status_pagamento(self, status):
+    def setStatusPagamento(self, status):
         self._status_pagamento = status
         return True
 
-    def faturar_pedido(self, cartao_credito):
+    def faturarPedido(self, cartao_credito):
         if cartao_credito.debitar(self._valor_total):
-            self.set_status_pagamento(status = True)
+            self.setStatusPagamento(status=True)
             return True
         else:
             return False
-    
+
     def estornar(self, pedido):
-        if self.get_numero() == pedido.get_numero():
-            self.set_status_pagamento(status = False)
+        if self.getNumero() == pedido.getNumero():
+            self.setStatusPagamento(status=False)
             return True
-        return False 
+        return False
--- models/produto.py
+++ models/produto.py
@@ -1,14 +1,14 @@
 class Produto:
-    def __init__(self, titulo, preço, estoque):
+    def __init__(self, titulo, preco, estoque):
         self._titulo = titulo
-        self._preço = preço
+        self._preco = preco
         self._estoque = estoque
         
     def getTitulo(self):
         return self._titulo
 
-    def getPreço(self):
-        return float(self._preço)
+    def getPreco(self):
+        return float(self._preco)
 
     def getEstoque(self):
         return self._estoque
